7b26fcff6a78cf12c3729f43f697728bcdc450df
gasali <waifudesu01@gmail.com>
2025-11-26 02:58:16 +0800

    Perbaikan 4

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,18 +1,17 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
+# 1. Tambahkan path project ke sistem agar Vercel tidak bingung
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
+
+# 2. Arahkan ke file settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+# 3. Jalankan aplikasi
 application = get_wsgi_application()
 
+# 4. Alias untuk Vercel (Wajib ada)
 app = application
